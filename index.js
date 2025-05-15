ab760f8002315da411af64801982fb68bd98bf36
jenwit-dev <johnny.js19@gmail.com>
2025-05-15 22:51:04 +0700

    learn:postman

--- index.js
+++ index.js
@@ -6,4 +6,15 @@ const student = {
   isAdmin: false,
   courses: ["html", "css", "js"],
   wife: null,
+  sayHi: function () {
+    console.log(this.name);
+  },
 };
+// console.log(typeof student);
+// console.log(typeof JSON.stringify(student));
+// student.sayHi();
+
+const studentJSON = JSON.stringify(student);
+console.log(JSON.parse(studentJSON));
+
+// https://jsonplaceholder.typicode.com
